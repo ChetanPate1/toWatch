fd72541f6b967e77d4b85daa21f4d698b5ed098c
Chetan Patel <chetanpate1@hotmail.co.uk>
2024-03-31 17:55:05 +0530

    gh page lib add for deployment

--- src/components/base/TwFormField.tsx
+++ src/components/base/TwFormField.tsx
@@ -12,7 +12,7 @@ type Props = {
 
 const TwFormField = (props: Props) => {
   const inputClass = classNames(
-    "relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 sm:text-sm text-gray-100 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500",
+    "relative block w-full appearance-none bg-transparent rounded-md border border-gray-300 px-3 py-2 sm:text-sm text-gray-100 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500",
     props.className
   );
 
