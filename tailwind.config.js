9da19269149ba827af2d562543ad6ea7684ab018
Chetan Patel <chetanpate1@hotmail.co.uk>
2024-06-04 18:45:05 +0100

    loader spin animation fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,10 +26,19 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        spinner: {
+          '0%': {
+            transform: 'rotate(0deg)'
+          },
+          '100%': {
+            transform: 'rotate(360deg)'
+          }
+        }
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "spinner": 'spinner 0.4s linear infinite'
       },
     },
   },
