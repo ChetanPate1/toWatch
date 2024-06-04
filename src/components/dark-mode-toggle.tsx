1a870282453beb040fc13fb05d3e77945c415d96
Chetan Patel <chetanpate1@hotmail.co.uk>
2024-06-04 22:57:51 +0100

    dark mode toggle z index fix

--- src/components/dark-mode-toggle.tsx
+++ src/components/dark-mode-toggle.tsx
@@ -16,7 +16,7 @@ export function DarkModeToggle() {
    return (
       <DropdownMenu>
          <DropdownMenuTrigger asChild>
-            <Button size="icon" className="fixed z-[1000] top-4 right-4">
+            <Button size="icon" className="fixed z-[50] top-4 right-4">
                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
                <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
                <span className="sr-only">Toggle theme</span>
