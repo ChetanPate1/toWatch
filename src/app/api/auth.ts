4b4ea824bb1e7e10f680e17f79af396971d52f03
Chetan Patel <chetanpate1@hotmail.co.uk>
2024-09-19 18:18:14 +0100

    save

--- src/app/api/auth.ts
+++ src/app/api/auth.ts
@@ -1,7 +1,7 @@
 // Local
-import { toast } from "@/components/ui/use-toast";
-import { storageUpdate } from "@/app/features/storageSlice";
-import { apiSlice } from "./";
+import { toast } from '@/components/ui/use-toast';
+import { storageUpdate } from '@/app/features/storageSlice';
+import { apiSlice } from './';
 
 const apiUrl = import.meta.env.VITE_APP_API_URL;
 
@@ -22,29 +22,29 @@ export const authApi = apiSlice.injectEndpoints({
     login: builder.mutation({
       query: (data: LoginPayload) => ({
         url: `${apiUrl}/login`,
-        method: "POST",
-        body: data,
+        method: 'POST',
+        body: data
       }),
       onQueryStarted: async (_, { dispatch, queryFulfilled }) => {
         try {
           const { data }: LoginResponse = await queryFulfilled;
 
-          await dispatch(storageUpdate({ prop: "token", value: data.token }));
-          await dispatch(storageUpdate({ prop: "email", value: data.email }));
+          await dispatch(storageUpdate({ prop: 'token', value: data.token }));
+          await dispatch(storageUpdate({ prop: 'email', value: data.email }));
           toast({
-            title: "Success!",
-            description: "You've been successfully logged in.",
+            title: 'Success!',
+            description: "You've been successfully logged in."
           });
         } catch (error) {
           console.log(error);
           toast({
-            title: "Uh oh! Something went wrong.",
-            description: "There was a problem with your request.",
+            title: 'Uh oh! Something went wrong.',
+            description: 'There was a problem with your request.'
           });
         }
-      },
-    }),
-  }),
+      }
+    })
+  })
 });
 
 export const { useLoginMutation } = authApi;
