3f885a257f0e0dfb6a1c184917a94ab43faf2f7c
Chetan Patel <chetanpate1@hotmail.co.uk>
2024-09-20 10:06:30 +0100

    colour fixes

--- src/components/TwNavigationBar.tsx
+++ src/components/TwNavigationBar.tsx
@@ -48,7 +48,7 @@ const TwNavigationBar = () => {
                 <>
                   {item.icon}
                   <span className="sr-only">{item.name}</span>
-                  <span className={`absolute h-1 w-full sm:h-full sm:w-1 rounded-full top-0 sm:-right-[1px] bg-indigo-500 dark:bg-white transition-transform duration-300 scale-y-0 group-hover:scale-y-100 ${isActive ? 'scale-y-100' : ''}`}></span>
+                  <span className={`absolute h-1 w-full sm:h-full sm:w-1 rounded-full top-0 sm:-right-[1px] bg-indigo-500 dark:bg-indigo-500 transition-transform duration-300 scale-y-0 group-hover:scale-y-100 ${isActive ? 'scale-y-100' : ''}`}></span>
                 </>
               )}
             </NavLink>
